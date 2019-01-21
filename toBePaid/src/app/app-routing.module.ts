894f8b1b08a603c57df8faea59451ed759b4f55f
654550265 <654450265@qq.com>
2019-01-21 21:34:45 +0800

    页面样式编写

--- toBePaid/src/app/app-routing.module.ts
+++ toBePaid/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
     {path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule'},
     {path: 'index', loadChildren: './index/index.module#IndexPageModule'},
     {path: 'task-details', loadChildren: './task-details/task-details.module#TaskDetailsPageModule'},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
 ];
 
 @NgModule({
--- toBePaid/src/app/tab2/tab2.page.ts
+++ toBePaid/src/app/tab2/tab2.page.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+    subList: Array<any>;
+
+    constructor() {
+        this.subList = [{
+            text: '全部订单',
+            type: '1',
+            isAcv: true
+        }, {
+            text: '待操作',
+            type: '2',
+            isAcv: false
+        }, {
+            text: '待返款',
+            type: '3',
+            isAcv: false
+        }, {
+            text: '已发货',
+            type: '4',
+            isAcv: false
+        }];
+    }
+
+    chooseOne(index) {
+        for (let values of this.subList) {
+            values.isAcv = false;
+        }
+        this.subList[index].isAcv = true;
+    }
+}
